87a5bd2255663b622dabe46154d8bfb02de6c078
zobekenobe <zobekenobe@gmail.com>
2022-12-13 00:03:49 +0000

    new edits

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+st.set_page_config('wide')
 project_list = [
     '',
     'Software Developer Salary Prediction',
@@ -13,11 +13,16 @@ option = st.sidebar.selectbox(options = project_list,label = 'Select an interest
 
 from PIL import Image
 image = Image.open('me.jpeg')
+st.title('My growing Portfolio of small machine learning projects')
 
+col1, col2 = st.columns([10,2])
+with col2:
+    st.image(image, width = 200)
+    st.caption('Zoheb Khan')
 
-st.image(image, width = 300 )
-st.header('Zoheb Khan')
-st.markdown("Hi, Im a computational research scientist working as Post-Doctoral research Fellow at the University College Dublin in the School of Chemical and Bioprocess Engineering")
-st.markdown("I use computational fluid dynamics and artificial intelligence to simulate and study flow fields in membrane separators")
-st.markdown("I love spending time outdoors and when not in the lab, I cycle, run and play badminton")
-st.markdown("To get in touch or know more about professional work, connect with me on LinkedIn @ https://www.linkedin.com/in/zobekenobe/")
+with col1:
+    st.markdown("Hi there, welcome to my portfolio")
+    st.markdown("Im a computational research scientist working as Post-Doctoral Research Fellow at the University College Dublin")
+    st.markdown("I use computational fluid dynamics and artificial intelligence to simulate and study flow fields in membrane separators")
+    st.markdown("I love spending time outdoors and when not in the lab, I cycle, run and play badminton")
+    st.markdown("To get in touch or know more about professional work, connect with me on [LinkedIn](https://www.linkedin.com/in/zobekenobe/)")
